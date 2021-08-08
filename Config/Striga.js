d78e684faac9d8d5b6ba53016d74e8383067a24c
K4ylexX <87926097+K4ylexX@users.noreply.github.com>
2021-08-09 00:10:13 +0300

    Add files via upload

---
+++ Config/Striga.js
@@ -0,0 +1,15 @@
+const striga = {
+serverID: "809091842691432489",//Sunucu idsi kanks
+token: "ODczNDgzMjY3NzI1MjIxOTI4.YQ5Erw.1En-xY6H7BtXD3TENxUiv6FxRwI",//Botun tokeni kanks
+ownerID: "345847333113692160", //Bot sahibinin idsi knaks
+prefix: [".","!"], //prefixler "!ceza bilgi 20" falan fıstık kanks
+voiceID: "873483703924441119", //Botun hangi ses kanalına girecekse o ses kanalının IDsi kanks
+
+sunucuTag: "✧",//Sunucunun Tagı İle Takılan Üyelerin Tagları Anladın Kanks yani sunucunun tagı kanks
+tagsızTag: "✧",//Sunucunun Tagsız Takılan Üyelerinin Tagları Anladın kanks
+
+statusText: "K4ylex ❤️ AESTHETIC", //Kafana göre kanks
+status: "dnd" //Botun çevirim içi kısmı kanks - dnd-online-idle-invisible
+} 
+
+module.exports = striga
